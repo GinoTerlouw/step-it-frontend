0ff9271a32f50ef4d0962a13d86d5c1a1fcb4d29
GinoT <ginod.98@hotmail.com>
2019-03-18 15:27:03 +0100

    created unit test for AppComponent and LoginComponent

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,12 +10,12 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   public userForm = new FormGroup({
-    email: new FormControl('test@test.com', [
+    email: new FormControl('', [
       Validators.required,
       Validators.email,
       Validators.maxLength(255)
     ]),
-    password: new FormControl('test123', [
+    password: new FormControl('', [
       Validators.required,
       Validators.minLength(6),
       Validators.maxLength(255)
