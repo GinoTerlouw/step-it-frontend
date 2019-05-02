9191dbcf2cbc63a1d4f91c114b482d4fdfe1c072
Mike Fels <mikefels@hotmail.com>
2019-05-02 14:05:25 +0200

    progress-circle component created

---
+++ src/app/components/ng-circle-progress/ng-circle-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-circle-progress',
+  templateUrl: './ng-circle-progress.component.html',
+  styleUrls: ['./ng-circle-progress.component.css']
+})
+export class NgCircleProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
